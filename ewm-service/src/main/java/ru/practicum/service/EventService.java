ecce0085f64752180a52bdd4c317cfceb13007d7
Maslova Olga <olga.maslova56@yandex.ru>
2023-08-01 16:12:26 +0300

    feax "с богом - вроде все работает v1.2"

--- ewm-service/src/main/java/ru/practicum/service/EventService.java
+++ ewm-service/src/main/java/ru/practicum/service/EventService.java
@@ -3,7 +3,6 @@ package ru.practicum.service;
 import ru.practicum.dto.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventService {
