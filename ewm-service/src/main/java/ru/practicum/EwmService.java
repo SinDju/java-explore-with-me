7fb3c6b8d254d8570709b871c59a29a8469695a6
Maslova Olga <olga.maslova56@yandex.ru>
2023-08-01 07:47:15 +0300

    feax "делаем архитектуру main service v1.26"

--- ewm-service/src/main/java/ru/practicum/EwmService.java
+++ ewm-service/src/main/java/ru/practicum/EwmService.java
@@ -2,12 +2,19 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ru.practicum.service", "ru.practicum.client"})
 public class EwmService {
     public static void main(String[] args) {
         SpringApplication.run(EwmService.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
